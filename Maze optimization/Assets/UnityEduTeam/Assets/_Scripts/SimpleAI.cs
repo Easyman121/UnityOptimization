8aa26c539981ff0169f4ea9b1010f5cb65bfdb6f
Easyman121 <2854470@gmail.com>
2024-12-20 02:09:12 +0300

    optimized script

--- Maze optimization/Assets/UnityEduTeam/Assets/_Scripts/SimpleAI.cs
+++ Maze optimization/Assets/UnityEduTeam/Assets/_Scripts/SimpleAI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
@@ -12,7 +11,6 @@ public class SimpleAI : MonoBehaviour {
     public LayerMask playerMask;
     public LayerMask obstacleMask;
 
-    [Space(5)]
     [Header("Agent Properties")]
     public float runSpeed;
     public float walkSpeed;
@@ -20,154 +18,124 @@ public class SimpleAI : MonoBehaviour {
 
     private NavMeshAgent agent;
     private Animator anim;
-
     private Transform playerTarget;
-
     private Vector3 currentDestination;
-
     private bool playerSeen;
 
-    private enum State {Wandering, Chasing};
+    private enum State { Wandering, Chasing }
     private State currentState;
 
-
-    // Use this for initialization
-    void Start () {
+    void Start() {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
-        currentDestination = RandomNavSphere(transform.position, patrolRadius, -1);
+        SetRandomDestination();
+        currentState = State.Wandering;
+
+        // Запускаем корутину
+        StartCoroutine(UpdateAIState());
     }
 
-    private void CheckState()
-    {
+    // Корутина для обновления состояния через интервалы
+    IEnumerator UpdateAIState() {
+        while (true) {
+            CheckState();
+
+            if (playerSeen) {
+                currentState = State.Chasing;
+            } else {
+                currentState = State.Wandering;
+            }
+
+            // Ждём 0.2 секунды перед следующим обновлением
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    private void CheckState() {
         FindVisibleTargets();
 
-        switch(currentState)
-        {
-            case State.Chasing:
-                ChaseBehavior();
-                break;
-
-            default:
-                WanderBehavior();
-                break;
-
+        if (currentState == State.Chasing) {
+            ChaseBehavior();
+        } else {
+            WanderBehavior();
         }
     }
 
-    void WanderBehavior()
-    {
-        anim.SetTrigger("walk");
+    private void WanderBehavior() {
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending) {
+            SetRandomDestination();
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run")) {
+            anim.SetTrigger("walk");
+        }
+
         agent.speed = walkSpeed;
-
-        float dist = agent.remainingDistance;
-
-        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete)
-        {
-            currentDestination = RandomNavSphere(transform.position, patrolRadius, -1);
-            agent.SetDestination(currentDestination);
-        }
-
     }
 
-    void ChaseBehavior()
-    {
-        if (playerTarget != null)
-        {
-            anim.SetTrigger("run");
+    private void ChaseBehavior() {
+        if (playerTarget != null) {
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Run")) {
+                anim.SetTrigger("run");
+            }
+
             agent.speed = runSpeed;
-            currentDestination = playerTarget.transform.position;
-            agent.SetDestination(currentDestination);
-        }
-        else
-        {
+            agent.SetDestination(playerTarget.position);
+        } else {
             playerSeen = false;
             currentState = State.Wandering;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
+    private void SetRandomDestination() {
+        currentDestination = RandomNavSphere(transform.position, patrolRadius, -1);
+        agent.SetDestination(currentDestination);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
             SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
     #region Vision
-    void FindVisibleTargets()
-    {
+    private void FindVisibleTargets() {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
 
         playerTarget = null;
         playerSeen = false;
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-        if (players.Length == 0)
-        {
-            return;
-        }
-        else
-        {
-            //Debug.Log("Found Player");
-        }
+        foreach (var target in targetsInViewRadius) {
+            Transform targetTransform = target.transform;
+            Vector3 dirToTarget = (targetTransform.position - transform.position).normalized;
 
-        foreach (GameObject player in players)
-        {
-            Vector3 dirToTarget = (player.transform.position - transform.position).normalized;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, dirToTarget, out hit))
-            {
-                float dstToTarget = Vector3.Distance(transform.position, player.transform.position);
-                if (dstToTarget <= viewRadius)
-                {
-                    if (Vector3.Angle(transform.forward, dirToTarget) <= viewAngle / 2)
-                    {
-                        if (hit.collider.tag == "Player")
-                        {
-                            playerSeen = true;
-                            playerTarget = hit.transform;
-                        }
-                    }
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) {
+                float distanceToTarget = Vector3.Distance(transform.position, targetTransform.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, obstacleMask)) {
+                    playerSeen = true;
+                    playerTarget = targetTransform;
+                    return;
                 }
             }
         }
     }
 
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
-    {
-        if (!angleIsGlobal)
-        {
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        if (!angleIsGlobal) {
             angleInDegrees += transform.eulerAngles.y;
         }
 
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 
-    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
-    {
-        Vector3 randDirection = Random.insideUnitSphere * dist;
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layerMask) {
+        Vector3 randomDirection = Random.insideUnitSphere * distance;
+        randomDirection += origin;
 
-        randDirection += origin;
-
-        NavMeshHit navHit;
-
-        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+        NavMesh.SamplePosition(randomDirection, out NavMeshHit navHit, distance, layerMask);
 
         return navHit.position;
     }
-
     #endregion
-
-    // Update is called once per frame
-    void Update () {
-        CheckState();
-
-        if (playerSeen)
-        {
-            currentState = State.Chasing;
-        } else
-        {
-            currentState = State.Wandering;
-        }
-	}
 }
